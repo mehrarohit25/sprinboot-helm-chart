93572ab35ea52353c2e102f47d2b11c7b99c361c
Rohit Mehra <mehrarohituk@gmail.com>
2022-07-18 11:00:55 +0100

    call to get of other installed application.

--- demoweb/src/main/java/com/example/demoweb/DemowebController.java
+++ demoweb/src/main/java/com/example/demoweb/DemowebController.java
@@ -4,6 +4,7 @@ package com.example.demoweb;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/")
@@ -11,7 +12,10 @@ public class DemowebController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello World.";
+        RestTemplate restTemplate = new RestTemplate();
+        String s= restTemplate.getForObject("http://34.171.207.92",String.class);
+        System.out.println(s);
+        return s+ "    Hello World New !!.";
     }
 
 }
